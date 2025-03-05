0edc282aa1d5a8fdd5e71e96bbf6466f1c8229a5
Bertók Attila <bertok.attila.21i@egressy.info>
2025-03-05 12:25:32 +0100

    reg/bejelent mukszik

--- project2025/Window2.xaml.cs
+++ project2025/Window2.xaml.cs
@@ -42,20 +42,28 @@ namespace project2025
                         name = textbox1.Text,
                         password = textbox2.Text
                     };
-
-                    string jsonData = JsonConvert.SerializeObject(jsonObject);
-                    StringContent data = new StringContent(jsonData, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = await client.PostAsync(url, data);
-                    string stringResponse = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringResponse);
-                    if (jsonResponse.message == "sikertelen regisztráció")
+                    if (textbox1.Text != "" && textbox2.Text != "")
                     {
-                        MessageBox.Show("Sikertelen regisztrácio");
+                        string jsonData = JsonConvert.SerializeObject(jsonObject);
+                        StringContent data = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                        HttpResponseMessage response = await client.PostAsync(url, data);
+                        string stringResponse = await response.Content.ReadAsStringAsync();
+                        dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(stringResponse);
+                        if (response.IsSuccessStatusCode && jsonResponse.message == "sikeres regisztráció")
+                        {
+                            MessageBox.Show("Sikeres regisztrácio");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sikeres regisztrácio");
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Sikeres regisztrácio");
+                        MessageBox.Show("Felhasználó n év vagy jelszó hiányos");
                     }
+
+                    
                 }
                 catch (Exception error)
                 {
